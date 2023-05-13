c4110ae23d6c73e99ee72d3cd1e388d2b0c7a589
Caio Eduardo <caio-boos>
2023-05-12 21:08:26 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+﻿function showItem(selector){
+    if(document.querySelector(selector) != undefined){
+        document.querySelector('.active').classList.remove('active');
+        document.querySelector(selector).classList.add('active');
+    }
+}
